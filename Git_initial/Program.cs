1bc7002e302a8fde8620ffd7c8fca90440c75ae9
denemeler <mkurt@eskisehir.edu.tr>
2021-12-19 15:52:11 +0300

    first commit

---
+++ Git_initial/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Git_initial
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today= new DateTime(2021,12,19);
+            Console.WriteLine("Hello Github! \n\rFrom now on, we are going to meet everyday, and grow up together.");
+            Console.WriteLine("😎❤️😍");
+            Console.WriteLine("Heyyy, it's the " + (DateTime.Today - today).TotalDays + ". day already 😎😎");
+        }
+    }
+}
